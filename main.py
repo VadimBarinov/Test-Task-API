55bbeb96a1b00c1ac60ce84b160f560cb189ae65
VadimBarinov <barinovvadim87@gmail.com>
2025-07-29 11:27:03 +0400

    swagger docs

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from database import get_db
 import schemas
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Тестовое задание MediaSoft",
+)
 
 
 @app.exception_handler(RequestValidationError)
@@ -27,7 +29,22 @@ async def validation_exception_handler(request, exc):
 
 
 @app.post('/city/')
-async def create_city(city: schemas.City, db: Session = Depends(get_db)) -> Any:
+async def create_city(city: schemas.City, db: Session = Depends(get_db)) -> PlainTextResponse:
+    """
+        # Создание города
+
+        ## Args:
+
+            city:
+                id: int
+                name: str
+
+            db: Сессия базы данных
+
+        ## Returns:
+
+            ID созданного города
+    """
     create_city_query = text("""
         INSERT INTO cities ("name")
         VALUES ('%s')
@@ -36,11 +53,22 @@ async def create_city(city: schemas.City, db: Session = Depends(get_db)) -> Any:
     city_id = db.execute(create_city_query).scalar()
     db.commit()
 
-    return city_id
+    return PlainTextResponse(("ID of the created object: " + str(city_id)), status_code=200)
 
 
 @app.get("/city/", response_model=List[schemas.City])
 async def get_all_cities(db: Session = Depends(get_db)) -> Any:
+    """
+        # Получение всех городов
+
+        ## Args:
+
+            db: Сессия базы данных
+
+        ## Returns:
+
+            Список всех городов
+    """
     get_cities_query = text("""
         SELECT * FROM cities;
     """)
@@ -49,7 +77,23 @@ async def get_all_cities(db: Session = Depends(get_db)) -> Any:
 
 
 @app.post('/city//street/')
-async def create_street(street: schemas.Street, db: Session = Depends(get_db)) -> Any:
+async def create_street(street: schemas.Street, db: Session = Depends(get_db)) -> PlainTextResponse:
+    """
+        # Создание улицы в указанном города
+
+        ## Args:
+
+            street:
+                id: int
+                name: str
+                city_id: int
+
+            db: Сессия базы данных
+
+        ## Returns:
+
+            ID созданной улицы
+    """
     get_cities_query = text("""
         SELECT COUNT(*) FROM cities 
         WHERE id = %s;
@@ -74,11 +118,24 @@ async def create_street(street: schemas.Street, db: Session = Depends(get_db)) -
         street_id = db.execute(create_street_query).scalar()
         db.commit()
 
-        return street_id
+        return PlainTextResponse(("ID of the created object: " + str(street_id)), status_code=200)
 
 
 @app.get('/city//street/', response_model=List[schemas.Street])
-async def get_all_streets_in_this_city(city_id: int, db: Session = Depends(get_db)) -> Any:
+async def get_all_streets_of_the_city(city_id: int, db: Session = Depends(get_db)) -> Any:
+    """
+        # Получение всех улиц города
+
+        ## Args:
+
+            city_id: int
+
+            db: Сессия базы данных
+
+        ## Returns:
+
+            Список всех улиц города
+    """
     get_cities_query = text("""
         SELECT COUNT(*) FROM cities 
         WHERE id = %s;
@@ -99,7 +156,27 @@ async def get_all_streets_in_this_city(city_id: int, db: Session = Depends(get_d
 
 
 @app.post('/shop/')
-async def create_shop(shop: schemas.Shop, db: Session = Depends(get_db)) -> Any:
+async def create_shop(shop: schemas.Shop, db: Session = Depends(get_db)) -> PlainTextResponse:
+    """
+        # Создание магазина
+
+        ## Args:
+
+            shop:
+                id: int
+                name: str
+                city_id: int
+                street_id: int
+                house: int
+                opening_time: str (00:00:00)
+                closing_time: str (23:59:59)
+
+            db: Сессия базы данных
+
+        ## Returns:
+
+            ID созданного магазина
+    """
     create_shop_query = text("""
         INSERT INTO shops (
             name,
@@ -125,32 +202,51 @@ async def create_shop(shop: schemas.Shop, db: Session = Depends(get_db)) -> Any:
     shop_id = db.execute(create_shop_query).scalar()
     db.commit()
 
-    return shop_id
+    return PlainTextResponse(("ID of the created object: " + str(shop_id)), status_code=200)
 
 
 @app.get('/shop/', response_model=List[schemas.ShopGet])
 async def get_all_shops_with_param(
-        street: int = None, city: int = None, open: int = None, db:Session = Depends(get_db)
+        street_id: int = None, city_id: int = None, is_open: int = None, db:Session = Depends(get_db)
 ) -> Any:
-    if street and city and open and (street < 0 or city < 0 or open < 0 or open > 1):
+    """
+        # Получение списка магазинов
+        Метод принимает параметры для фильтрации. Параметры не обязательны. В случае отсутствия параметров выводятся все магазины, если хоть один параметр есть, то по нему выполняется фильтрация.
+
+        ## Args:
+
+            street_id: int
+            city_id: int
+            is_open: int (0 - закрыт, 1 - открыт)
+            (Данный статус определяется исходя из параметров
+            «Время открытия», «Время закрытия» и текущего
+            времени сервера)
+
+            db: Сессия базы данных
+
+        ## Returns:
+
+            Список магазинов удовлетворяющих параметрам
+    """
+    if (is_open and (is_open < 0 or is_open > 1)) or (city_id and city_id < 0) or (street_id and street_id < 0):
         raise HTTPException(status_code=400, detail="Incorrect data has been entered.")
 
     else:
 
-        if street:
+        if street_id:
             get_streets_query = text("""
                 SELECT COUNT(*) FROM streets 
                 WHERE id = %s;
-            """ % (street, ))
+            """ % (street_id,))
             street_is_exists = db.execute(get_streets_query)
             if street_is_exists.scalar() < 1:
                 raise HTTPException(status_code=400, detail="There is no street with such an ID.")
 
-        if city:
+        if city_id:
             get_cities_query = text("""
                 SELECT COUNT(*) FROM cities 
                 WHERE id = %s;
-            """ % (city, ))
+            """ % (city_id,))
             city_is_exists = db.execute(get_cities_query)
             if city_is_exists.scalar() < 1:
                 raise HTTPException(status_code=400, detail="There is no city with such an ID.")
@@ -169,10 +265,10 @@ async def get_all_shops_with_param(
                 AND streets.city_id = cities.id
                 %s %s %s %s;
         """ % (
-            ("AND shops.city_id = %s" % (city, ))if city else "",
-            ("AND shops.street_id = %s" % (street, )) if street else "",
-            "AND CURRENT_TIME BETWEEN shops.opening_time AND shops.closing_time" if open == 1 else "",
-            "AND CURRENT_TIME NOT BETWEEN shops.opening_time AND shops.closing_time" if open == 0 else "",
+            ("AND shops.city_id = %s" % (city_id,))if city_id else "",
+            ("AND shops.street_id = %s" % (street_id,)) if street_id else "",
+            "AND CURRENT_TIME BETWEEN shops.opening_time AND shops.closing_time" if is_open == 1 else "",
+            "AND CURRENT_TIME NOT BETWEEN shops.opening_time AND shops.closing_time" if is_open == 0 else "",
         ))
 
         return db.execute(get_shops_query)
